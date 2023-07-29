a25a8bd04b37e294868be530d574a33e9779d388
Rodrigo Santos <rsantosls18@gmail.com>
2023-07-29 13:41:21 -0300

    Adição do Header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { HeaderLayout } from "./layout/HeaderLayout";
 import { HomePage } from "./pages/HomePage";
 
 function App() {
   return (
     <div className="App">
+      <HeaderLayout/>
       <HomePage/>
     </div>
   );
